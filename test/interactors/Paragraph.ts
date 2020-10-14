c703e20232b5cd1edb22d49f66baeb0cdcbde746
minkimcello <minkimcello@gmail.com>
2020-10-14 15:05:34 -0400

    Env in bigtest.json

--- test/interactors/Paragraph.ts
+++ test/interactors/Paragraph.ts
@@ -2,8 +2,5 @@ import { createInteractor } from '@bigtest/interactor';
 
 export default createInteractor<HTMLElement>('p')({
   selector: 'p',
-  locator: element => element.textContent,
-  filters: {
-    id: element => element.id
-  }
+  locator: element => element.textContent || '',
 });
