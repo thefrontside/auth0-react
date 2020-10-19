ec20c185bc7fd67ec60a2ea9621d83426c4e960a
minkimcello <minkimcello@gmail.com>
2020-10-19 09:57:24 -0400

    Export generate code for user

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { Auth0SimulationProvider } from './provider';
 export { checkAuth0Simulation } from './check-auth0-simulation';
 export { AuthorizeAuth0 } from './test-helpers';
+export { generateCodeForUser } from './generate-code-for-user';
